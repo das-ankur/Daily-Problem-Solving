e016d0a88a90358511ad98e4d369ff6fba541b5c
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-02-17 13:46:39 +0530

    Time: 17 ms (64.39%), Space: 10.9 MB (29.07%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,56 +1,16 @@
 class Solution {
 public:
-    /*
-    // Brute Force
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res(2);
         int n=nums.size();
-        bool flag=false;
+        unordered_map<int, int> m;
+        vector<int> res(2);
         for(int i=0;i<n;i++) {
-            for(int j=i+1;j<n;j++) {
-                if(nums[i]+nums[j] == target) {
-                    flag=true;
-                    res[0]=i;
-                    res[1]=j;
-                    break;
-                }
-            }
-            if(flag)
+            if(m.find(target-nums[i])!=m.end()) {
+                res[0]=i;
+                res[1]=m[target-nums[i]];
                 break;
-        }
-        return res;
-    }
-    */
-    /*
-    // Two-pass Hash Table
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res={-1, -1};
-        unordered_map<int, int> map;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-            map[nums[i]]=i;
-        for(int i=0;i<n;i++) {
-            int comp=target-nums[i];
-            if(map.count(comp) && map[comp]!=i)  {
-                res[0]=map[comp];
-                res[1]=i;
             }
-        }
-        return res;
-    }
-    */
-    // One-pass Hash Table
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res(2);
-        unordered_map<int, int> map;
-        int n=nums.size();
-        for(int i=0;i<n;i++) {
-            int comp=target-nums[i];
-            if(map.find(comp)!=map.end())  {
-                res[0]=map[comp];
-                res[1]=i;
-            }
-            map[nums[i]]=i;
+            m[nums[i]]=i;
         }
         return res;
     }
