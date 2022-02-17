22cbd555334bad32fe6af6c676c2eb1c637eb6e0
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-02-18 02:28:06 +0530

    Time: 10 ms (82.01%), Space: 18.3 MB (93.33%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i=0, j=1, n=nums.size();
-        if(n==0)
-            return 0;
-        // Apply two pointers approach
-        // i is used for traversing
-        // j is used to traverse maintain the index of unique elements
-        for(;i<n;i++) {
-            if(nums[i]!=nums[j-1]) {
-                nums[j++]=nums[i];
-            }
+        int i=0,j=0;
+        while(j<nums.size()) {
+            if(nums[i]==nums[j])
+                j++;
+            else 
+                nums[++i]=nums[j++];
         }
-        return j;
+        return i+1;
     }
 };
