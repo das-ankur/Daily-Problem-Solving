f3c31cfec8bd73caad8bb9044cea8098dbcb1b0b
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-02-20 13:01:04 +0530

    Time: 5 ms (9.77%), Space: 6.2 MB (28.81%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    /*
+    //Time Limit Exceed
+    int climb(int n, int step) {
+        n-=step;
+        if(n==0)
+            return 1;
+        if(n<0)
+            return 0;
+        return climb(n, 1)+climb(n, 2);
+    }
+    int climbStairs(int n) {
+        long  long int count = 0;
+        count+=climb(n, 1);
+        count+=climb(n, 2);
+        return count;
+    }
+    */
+    int climbStairs(int n) {
+        vector<int> table(n+1);
+        if(n<=2)
+            return n;
+        table[0]=0;
+        table[1]=1;
+        table[2]=2;
+        for(int i=3;i<=n;i++)
+            table[i]=table[i-1]+table[i-2];
+        return table[n];
+    }
+};
