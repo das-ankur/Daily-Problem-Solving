595311adbd5eae30f875e535c382125c4240e2c4
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-02-23 18:13:42 +0530

    Time: 17 ms (10.87%), Space: 10 MB (83.87%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int> res;
         unordered_map<int, int>m1;
@@ -13,6 +14,39 @@ public:
             if(it2!=m2.end()) {
                 for(int i=0;i<min(it1->second, it2->second);i++)
                     res.push_back(it1->first);
+            }
+        }
+        return res;
+    }*/
+    bool binarySearch(vector<int>& nums, int target) {
+        int low=0, high=nums.size()-1, mid;
+        while(low<=high) {
+            mid=(high+low)/2;
+            if(nums[mid]==target) {
+                nums.erase(nums.begin()+mid);
+                return true;
+            }
+            else if(nums[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return false;
+    }
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        if(nums1.size()<nums2.size()) {
+            sort(nums2.begin(), nums2.end());
+            for(int i=0;i<nums1.size();i++) {
+                if(binarySearch(nums2, nums1[i]))
+                    res.push_back(nums1[i]);
+            }
+        }
+        else {
+            sort(nums1.begin(), nums1.end());
+            for(int i=0;i<nums2.size();i++) {
+                if(binarySearch(nums1, nums2[i]))
+                    res.push_back(nums2[i]);
             }
         }
         return res;
