0132f5976a37f5b500f3e47f4ed1ce4432e77f72
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-02-23 10:44:49 +0530

    Time: 6 ms (71.39%), Space: 10.1 MB (77.70%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        /*
+        // Using unordered_map
         unordered_map<int , int> m1;
         unordered_map<int , int> m2;
         for(int i=0;i<nums1.size();i++)
@@ -11,6 +13,17 @@ public:
         for(auto it=m1.begin();it!=m1.end();it++) {
             if(m2.find(it->first)!=m2.end())
                 res.push_back(it->first);
+        }
+        return res;
+        */
+        // Using binary serach
+        vector<int> res;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        for(int i=0;i<nums1.size();i++) {
+            if(binary_search(nums2.begin(), nums2.end(), nums1[i]))
+                if(res.size()==0 || res[res.size()-1]!=nums1[i])
+                    res.push_back(nums1[i]);
         }
         return res;
     }
