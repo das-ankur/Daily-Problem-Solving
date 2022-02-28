cf36a0c343b72e8bc989c5e80c1b369f7b7b1296
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-02-28 13:56:26 +0530

    Added solution - LeetHub

---
+++ Combination Sum - GFG/combination-sum.cpp
@@ -0,0 +1,68 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    //Function to return a list of indexes denoting the required 
+    //combinations whose sum is equal to given number.
+    void findNumbers(vector<int> &A, long long int sum, vector<vector<int>> &res, vector<int> &r, long long int i) {
+        if(sum==0) {
+            res.push_back(r);
+            return;
+        }
+        while(i<A.size() && sum-A[i]>=0) {
+            r.push_back(A[i]);
+            findNumbers(A, sum-A[i], res, r, i);
+            i++;
+            r.pop_back();
+        }
+    }
+    vector<vector<int> > combinationSum(vector<int> &A, int B) {
+        sort(A.begin(), A.end());
+        A.erase(unique(A.begin(), A.end()), A.end());
+        vector<vector<int>> res;
+        vector<int> r;
+        findNumbers(A, B, res, r, 0);
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> A;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            A.push_back(x);
+        }   
+        int sum;
+        cin>>sum;
+        Solution ob;
+        vector<vector<int>> result = ob.combinationSum(A, sum);
+   		if(result.size()==0){
+   			cout<<"Empty";
+   		}
+        for(int i=0;i<result.size();i++){
+            cout<<'(';
+            for(int j=0;j<result[i].size();j++){
+                cout<<result[i][j];
+                if(j<result[i].size()-1)
+                    cout<<" ";
+            }
+            cout<<")";
+        }
+        cout<<"\n";
+    }
+}	  // } Driver Code Ends
