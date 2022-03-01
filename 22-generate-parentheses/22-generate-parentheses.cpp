0b42e965676af10827403d8eb53b7ac79f434cde
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-03-01 16:29:13 +0530

    Time: 15 ms (15.18%), Space: 16 MB (14.73%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     vector<string> valid;
     void generate(string &s, int open, int close) {
         if(open==0 && close==0) {
@@ -16,6 +17,30 @@ public:
                 s.push_back(')');
                 generate(s, open, close-1);
                 s.pop_back();
+            }
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        string s;
+        generate(s, n, n);
+        return valid;
+    }*/
+    vector<string> valid;
+    void generate(string s, int open, int close) {
+        if(open==0 && close==0) {
+            valid.push_back(s);
+            return;
+        }
+        if(open>0) {
+            s.push_back('(');
+            generate(s, open-1, close);
+            s.pop_back();
+        }
+        if(close>0) {
+            if(open<close) {
+                s.push_back(')');
+                generate(s, open, close-1);
+                //s.pop_back();
             }
         }
     }
