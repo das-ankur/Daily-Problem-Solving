5d1a268ab73ab2bc5010403c6ec58d4a04c347e8
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-03-25 01:36:15 +0530

    Time: 135 ms (78.35%), Space: 93.4 MB (13.00%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        /*
-        // Brute Force
-        // Time COmplexcity: O(n^2)
+        // Initial buying price is prices[0]
+        int buy=prices[0];
         int profit=0;
-        int n=prices.size();
-        for(int i=0;i<n;i++) {
-            for(int j=i+1;j<n;j++) {
-                int temp=prices[j]-prices[i];
-                if(temp>profit) 
-                    profit=temp;
-            }
+        //The main aim is:
+        // i) Minimize buying prize
+        // ii) Maximizing profit
+        for(int i=1;i<prices.size();i++) {
+            int dif=prices[i]-buy;
+            buy=buy<prices[i]?buy:prices[i];
+            profit=profit>dif?profit:dif;
         }
         return profit;
-        */
-        // Dynamic Programming : Explanation - https://leetcode.com/media/original_images/121_profit_graph.png
-        // Focus on the peak and valley of the graph and try to memorize maximum profit for each day
-        // Time Complexcity: O(n)
-        int minprice=INT_MAX;
-        int maxprice=0;
-        int n=prices.size();
-        for(int i=0;i<n;i++) {
-            if(prices[i]<minprice)
-                minprice=prices[i];
-            else if(prices[i]-minprice>maxprice)
-                maxprice=prices[i]-minprice;
-        }
-        return maxprice;
     }
 };
