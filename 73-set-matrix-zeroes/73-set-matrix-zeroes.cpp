2c27d640cc1c8c6672f7269ba8087672d3040bc4
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-04-14 01:32:48 +0530

    Time: 12 ms (91.68%), Space: 13.3 MB (53.94%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        /*
+        // Using Set
         set<int> cols;
         set<int> rows;
         int m=matrix.size(), n=matrix[0].size();
@@ -21,6 +23,25 @@ public:
             int c=*it;
             for(int k=0;k<m;k++)
                 matrix[k][c]=0;
+        }
+        */
+        // Taking two external dummy array
+        int m=matrix.size(), n=matrix[0].size();
+        vector<int> rows(m);
+        vector<int> cols(n);
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(matrix[i][j]==0) {
+                    rows[i]=1;
+                    cols[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++) {
+                if(rows[i]==1 || cols[j]==1)
+                    matrix[i][j]=0;
+            }
         }
     }
 };
