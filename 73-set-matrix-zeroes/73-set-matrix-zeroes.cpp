3f1026d53fc7c1ed7d4d1fbc8d9e3300a3acc234
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-04-14 01:50:34 +0530

    Time: 23 ms (36.56%), Space: 13.1 MB (82.01%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -25,6 +25,7 @@ public:
                 matrix[k][c]=0;
         }
         */
+        /*
         // Taking two external dummy array
         int m=matrix.size(), n=matrix[0].size();
         vector<int> rows(m);
@@ -42,6 +43,28 @@ public:
                 if(rows[i]==1 || cols[j]==1)
                     matrix[i][j]=0;
             }
+        }
+        */
+        //Fiest row and column as dummy  array
+        int m=matrix.size(), n=matrix[0].size();
+        bool col=false;
+        for(int i=0;i<m;i++) {
+            if(matrix[i][0]==0)
+                col=true;
+            for(int j=1;j<n;j++) {
+                if(matrix[i][j]==0) {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=m-1;i>=0;i--) {
+            for(int j=n-1;j>=1;j--) {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j]=0;
+            }
+            if(col)
+                matrix[i][0]=0;
         }
     }
 };
