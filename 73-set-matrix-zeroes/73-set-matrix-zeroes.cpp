7ccf531866478f1d79f3c7036ec514ac796c778b
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-04-14 23:07:11 +0530

    Time: 15 ms (77.88%), Space: 13 MB (95.83%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -45,7 +45,8 @@ public:
             }
         }
         */
-        //Fiest row and column as dummy  array
+        /*
+        //First row and column as dummy  array
         //Most Optimized in respect to time and space complexcity
         int m=matrix.size(), n=matrix[0].size();
         bool col=false;
@@ -61,6 +62,28 @@ public:
         }
         for(int i=m-1;i>=0;i--) {
             for(int j=n-1;j>=1;j--) {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j]=0;
+            }
+            if(col)
+                matrix[i][0]=0;
+        }
+        */
+        //Practice 1
+        int m=matrix.size(), n=matrix[0].size();
+        bool col=false;
+        for(int i=0;i<m;i++) {
+            if(matrix[i][0]==0)
+                col=true;
+            for(int j=1;j<n;j++) {
+                if(matrix[i][j]==0) {
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        for(int i=m-1;i>=0;i--) {
+            for(int j=n-1;j>0;j--) {
                 if(matrix[i][0]==0 || matrix[0][j]==0)
                     matrix[i][j]=0;
             }
