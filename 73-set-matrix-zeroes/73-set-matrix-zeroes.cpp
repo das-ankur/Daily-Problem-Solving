e2b4bf97a249969e7c26be83fa1e22a19bc03255
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-04-16 12:02:31 +0530

    Time: 10 ms (94.69%), Space: 13.2 MB (54.49%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -69,6 +69,7 @@ public:
                 matrix[i][0]=0;
         }
         */
+        /*
         //Practice 1
         int m=matrix.size(), n=matrix[0].size();
         bool col=false;
@@ -79,6 +80,28 @@ public:
                 if(matrix[i][j]==0) {
                     matrix[0][j]=0;
                     matrix[i][0]=0;
+                }
+            }
+        }
+        for(int i=m-1;i>=0;i--) {
+            for(int j=n-1;j>0;j--) {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j]=0;
+            }
+            if(col)
+                matrix[i][0]=0;
+        }
+        */
+        //Practice 2
+        int m=matrix.size(), n=matrix[0].size();
+        bool col=false;
+        for(int i=0;i<m;i++) {
+            if(matrix[i][0]==0)
+                col=true;
+            for(int j=1;j<n;j++) {
+                if(matrix[i][j]==0) {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
