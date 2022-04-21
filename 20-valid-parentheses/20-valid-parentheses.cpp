4eb9af426325965ab0d0c0e075e50f9b833d163c
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-04-21 23:15:08 +0530

    Time: 2 ms (42.44%), Space: 6.4 MB (10.13%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isValid(string s) {
+        /*
         stack<char> stk;
         bool flag=true;
         for(int i=0;i<s.size();i++) {
@@ -25,6 +26,36 @@ public:
         }
         if(!stk.empty())
             return false;
+        return flag;
+        */
+        stack<char> stk;
+        bool flag=true;
+        for(int i=0;i<s.size();i++) {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                stk.push(s[i]);
+            else {
+                if(s[i]==')') {
+                    if(stk.empty() || stk.top()=='{' || stk.top() == '[') {
+                        flag=false;
+                    }
+                }
+                else if(s[i]=='}') {
+                    if(stk.empty() || stk.top()=='(' || stk.top() == '[') {
+                        flag=false;
+                    }
+                }
+                if(s[i]==']') {
+                    if(stk.empty() || stk.top()=='{' || stk.top() == '(') {
+                        flag=false;
+                    }
+                }
+                stk.pop();
+            }
+            if(!flag)
+                break;
+        }
+        if(stk.size()!=0)
+            flag=false;
         return flag;
     }
 };
