66663c69cc8ca2a6e6ef739486d049e34d70c138
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-04-29 23:22:34 +0530

    Time: 146 ms (46.33%), Space: 67.8 MB (55.31%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -11,6 +11,7 @@ public:
         }
         return res;
         */
+        /*
         //Practice 1
         int res=INT_MIN, temp_sum=0;
         for(int i=0;i<nums.size();i++) {
@@ -18,6 +19,18 @@ public:
             if(temp_sum<nums[i])
                 temp_sum=nums[i];
             res=max(res, temp_sum);
+        }
+        return res;
+        */
+        //Practice 2
+        int max_end_here=0, res=INT_MIN;
+        for(int i=0;i<nums.size();i++) {
+            if(max_end_here+nums[i]<nums[i])
+                max_end_here=nums[i];
+            else
+                max_end_here+=nums[i];
+            if(res<max_end_here)
+                res=max_end_here;
         }
         return res;
     }
