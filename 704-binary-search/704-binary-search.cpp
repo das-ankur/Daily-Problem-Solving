df4fdcd452fe8c393f62daf50c4de9801aecab00
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-06-02 13:49:11 +0530

    Time: 44 ms (71.44%), Space: 27.6 MB (64.18%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -14,7 +14,21 @@ public:
         }
         return -1;
         */
+        /*
         //Practice 1
+        int low=0, high=nums.size()-1;
+        while(low<=high) {
+            int mid=(low+high)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return -1;
+        */
+        // Practice 2
         int low=0, high=nums.size()-1;
         while(low<=high) {
             int mid=(low+high)/2;
