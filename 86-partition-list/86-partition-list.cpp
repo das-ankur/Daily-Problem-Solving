e5ec924b926724132636a71bd503a4cfe79fe9dc
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-07-22 22:32:08 +0530

    Time: 6 ms (77.09%), Space: 10.5 MB (11.89%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -11,17 +11,25 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
+        // Using two queues
         if(head==NULL || head->next==NULL)
             return head;
         queue<ListNode *> less_part;
         queue<ListNode *> great_part;
         ListNode *ptr=head;
+        // Put all the elements which are less than x in less_part
+        // Put all the elements which are greater than x in great_part
         while(ptr!=NULL) {
             if(ptr->val<x) less_part.push(ptr);
             else great_part.push(ptr);
             ptr=ptr->next;
         }
+        // Create two pointr
+        // res will point the first node of the result
+        // last will point the last node the last node of the result throughout the execution 
         ListNode *res=NULL,*last=NULL;
+        // As we need elements less than x at the beginning of the list
+        // then first insert the elements of the less_part in FIFO order
         while(!less_part.empty()) {
             if(last==NULL) {
                 last=less_part.front();
@@ -33,6 +41,8 @@ public:
             }
             less_part.pop();
         }
+        // As we need elements greater than or equal to x at the end of the list
+        // then insert the elements of great_part
         while(!great_part.empty()) {
             if(last==NULL)  {
                 last=great_part.front();
