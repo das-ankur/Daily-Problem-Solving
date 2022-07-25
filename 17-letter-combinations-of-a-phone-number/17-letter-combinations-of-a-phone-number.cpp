cda6d591a382cac2fb6cd9389f33a1d3631d832b
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-07-25 22:36:41 +0530

    Time: 2 ms (52.03%), Space: 6.5 MB (57.43%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,97 @@
+class Solution {
+public:
+    /*
+    void helper(string &digits, int i, string &s, vector<string> &res, unordered_map<char, string> &m) {
+        if(i>=digits.size()) {
+            res.push_back(s);
+            return;
+        }
+        string temp=m[digits[i]];
+        for(int j=0;j<temp.size();j++) {
+            s.push_back(temp[j]);
+            helper(digits,i+1,s,res,m);
+            s.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        if(digits.empty())
+            return res;
+        else {
+            unordered_map<char, string> m;
+            m['2']="abc";
+            m['3']="def";
+            m['4']="ghi";
+            m['5']="jkl";
+            m['6']="mno";
+            m['7']="pqrs";
+            m['8']="tuv";
+            m['9']="wxyz";
+            string s;
+            helper(digits,0,s,res,m);
+        }
+        return res;
+    }
+    */
+    /*
+    // Practice 1
+    void helper(string &digits, int pos, vector<string> &res, string &s, unordered_map<char,string> &m) {
+        if(pos>=digits.size())  {
+            res.push_back(s);
+            return;
+        }
+        string temp=m[digits[pos]];
+        for(int j=0;j<temp.size();j++) {
+            s.push_back(temp[j]);
+            helper(digits,pos+1,res,s,m);
+            s.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        if(digits.empty())
+            return res;
+        unordered_map<char, string> m;
+        m['2']="abc";
+        m['3']="def";
+        m['4']="ghi";
+        m['5']="jkl";
+        m['6']="mno";
+        m['7']="pqrs";
+        m['8']="tuv";
+        m['9']="wxyz";
+        string s;
+        helper(digits,0,res,s,m);
+        return res;
+    }
+    */
+    void helper(string &digits,int pos,vector<string> &res, string &s,unordered_map<char,string> &m) {
+        if(pos>=digits.size()) {
+            res.push_back(s);
+            return;
+        }
+        string temp=m[digits[pos]];
+        for(int i=0;i<temp.size();i++) {
+            s.push_back(temp[i]);
+            helper(digits,pos+1,res,s,m);
+            s.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        if(digits.empty())
+            return res;
+        unordered_map<char,string> m;
+        m['2']="abc";
+        m['3']="def";
+        m['4']="ghi";
+        m['5']="jkl";
+        m['6']="mno";
+        m['7']="pqrs";
+        m['8']="tuv";
+        m['9']="wxyz";
+        string s;
+        helper(digits,0,res,s,m);
+        return res;
+    }
+};
