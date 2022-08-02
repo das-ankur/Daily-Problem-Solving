71aca87c3637af02b590f387dd241a7bac0bb7a6
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-08-02 11:56:42 +0530

    Time: 4 ms (87.55%), Space: 10.7 MB (73.59%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -25,6 +25,7 @@ public:
         return ans;
     }
     */
+    /*
     void helper(vector<int> &arr, int pos, int sum, int target, vector<int> &subset, vector<vector<int>> &res) {
         if(sum==target) {
             res.push_back(subset);
@@ -45,6 +46,30 @@ public:
         vector<int> subset;
         vector<vector<int>> res;
         helper(candidates,0,0,target,subset,res);
+        return res;
+    }
+    */
+    // Practice 2
+    void helper(int pos, vector<int> &arr, int sum, int target, vector<int> &subset, vector<vector<int>> &res) {
+        if(sum==target) {
+            res.push_back(subset);
+            return;
+        }
+        if(sum>target) return;
+        if(pos>=arr.size()) return;
+        subset.push_back(arr[pos]);
+        sum+=arr[pos];
+        helper(pos+1,arr,sum,target,subset,res);
+        subset.pop_back();
+        sum-=arr[pos];
+        while(pos+1<arr.size() && arr[pos]==arr[pos+1]) pos++;
+        helper(pos+1,arr,sum,target,subset,res);
+    }
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> res;
+        vector<int> subset;
+        helper(0,candidates,0,target,subset,res);
         return res;
     }
 };
