32251301658d665c9d18c87b0549b0b8724be6a4
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-08-02 12:12:16 +0530

    Time: 5 ms (19.61%), Space: 6.6 MB (55.14%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -96,6 +96,7 @@ public:
         return res;
     }
     */
+    /*
     // Practice 2
     void helper(string &digits, int pos, string &s, vector<string> &res, unordered_map<char,string> &m) {
         if(pos>=digits.size()) {
@@ -123,6 +124,67 @@ public:
         m['9']="wxyz";
         string s;
         helper(digits,0,s,res,m);
+        return res;
+    }
+    */
+    /*
+    //Practice 3
+    void helper(string &digits, int idx, string &s, unordered_map<char,string> &m, vector<string> &res) {
+        if(idx>=digits.size()) {
+            res.push_back(s);
+            return;
+        }
+        string temp=m[digits[idx]];
+        for(char ch:temp) {
+            s.push_back(ch);
+            helper(digits,idx+1,s,m,res);
+            s.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        if(digits.empty()) return res;
+        unordered_map<char,string> m;
+        m['2']="abc";
+        m['3']="def";
+        m['4']="ghi";
+        m['5']="jkl";
+        m['6']="mno";
+        m['7']="pqrs";
+        m['8']="tuv";
+        m['9']="wxyz";
+        string s;
+        helper(digits,0,s,m,res);
+        return res;
+    }
+    */
+    // Practice 4
+    void helper(string &s, int pos, string &subset, vector<string> &res, unordered_map<char,string> &m) {
+        if(pos>=s.size()) {
+            res.push_back(subset);
+            return;
+        }
+        string temp=m[s[pos]];
+        for(char ch:temp) {
+            subset.push_back(ch);
+            helper(s,pos+1,subset,res,m);
+            subset.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        if(digits.empty()) return res;
+        unordered_map<char,string> m;
+        m['2']="abc";
+        m['3']="def";
+        m['4']="ghi";
+        m['5']="jkl";
+        m['6']="mno";
+        m['7']="pqrs";
+        m['8']="tuv";
+        m['9']="wxyz";
+        string subset;
+        helper(digits,0,subset,res,m);
         return res;
     }
 };
