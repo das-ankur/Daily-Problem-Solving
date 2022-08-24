1cad9ec8bedb57bd435e5c8c1dcceeccda9767f0
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-08-24 20:07:00 +0530

    Time: 0 ms (100.00%), Space: 6 MB (93.32%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -23,6 +23,7 @@ public:
         return helper(n-1,s,memo);
     }
     */
+    /*
     // Tabulation
     int numDecodings(string s) {
         int n=s.size();
@@ -40,5 +41,24 @@ public:
             table[i]=one_digit+two_digit;
         }
         return table[n-1];
+    }*/
+    // Space Optimization
+    int numDecodings(string s) {
+        int n=s.size();
+        int prev2=0,prev1=1;
+        if(s[0]=='0') return 0;
+        for(int i=1;i<n;i++) {
+            int one_digit=0;
+            if(s[i]!='0') one_digit=prev1;
+            int two_digit=0;
+            if(i-1>=0 && s[i-1]=='1' || (s[i-1]=='2' && s[i]<='6')) {
+                if(i-2<0) two_digit=1;
+                else two_digit=prev2;
+            }
+            int cur=one_digit+two_digit;
+            prev2=prev1;
+            prev1=cur;
+        }
+        return prev1;
     }
 };
