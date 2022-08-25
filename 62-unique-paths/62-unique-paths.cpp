408f800f9376f572b8478ae6a63d2f8b871195c4
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-08-25 17:48:24 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (26.81%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -13,6 +13,7 @@ public:
         return helper(m-1,n-1);
     }
     */
+    /*
     // Memoization
     int helper(int row, int col, vector<vector<int>> &memo) {
         if(row==0 && col==0) return 1;
@@ -25,5 +26,21 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> memo(m, vector<int>(n,-1));
         return helper(m-1,n-1,memo);
+    }
+    */
+    // Tabulation
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> table(m, vector<int>(n,-1));
+        table[0][0]=1;
+        for(int row=0;row<m;row++) {
+            for(int col=0;col<n;col++) {
+                if(row==0 && col==0) continue;
+                int move_left=0, move_up=0;
+                if(col>=1) move_left=table[row][col-1];
+                if(row>=1) move_up=table[row-1][col];
+                table[row][col]=move_left+move_up;
+            }
+        }
+        return table[m-1][n-1];
     }
 };
