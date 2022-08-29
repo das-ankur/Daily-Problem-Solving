66747966b263f9b181651ad60912a544245ca9cc
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-08-30 01:48:28 +0530

    Time: 28 ms (31.59%), Space: 10.2 MB (54.67%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -18,6 +18,7 @@ public:
         return path;
     }
     */
+    /*
     // mamoization
     int th=(int)(-1e9);
     int helper(int row, int col, int m, int n, vector<vector<int>> &matrix, vector<vector<int>> &memo) {
@@ -35,6 +36,26 @@ public:
         vector<vector<int>> memo(m, vector<int>(n,th));
         int path=INT_MAX;
         for(int j=0;j<n;j++) path=min(path, helper(m-1,j,m,n,matrix,memo));
+        return path;
+    }
+    */
+    // Tabulation
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>> table(m, vector<int>(n,1e9));
+        for(int col=0;col<n;col++) table[0][col]=matrix[0][col];
+        for(int row=1;row<m;row++) {
+            for(int col=0;col<n;col++) {
+                int up=matrix[row][col]+table[row-1][col];
+                int up_left=INT_MAX, up_right=INT_MAX;
+                if(col-1>=0) up_left=matrix[row][col]+table[row-1][col-1];
+                if(col+1<n) up_right=matrix[row][col]+table[row-1][col+1];
+                table[row][col]=min(up, min(up_left, up_right));
+            }
+        }
+        int path=INT_MAX;
+        for(int col=0;col<n;col++) path=min(path, table[m-1][col]);
         return path;
     }
 };
