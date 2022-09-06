267e433ddf074c02b1cc6c674cf2c5b4b5c22170
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-09-07 01:03:47 +0530

    Time: 15 ms (80.09%), Space: 12.3 MB (38.45%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    /*
+    // Recursion
+    int helper(int idx, int t, vector<int> &nums) {
+        if(idx==0) {
+            if(nums[idx]==t && nums[idx]==-t) return 2;
+            else if (nums[idx]==t || nums[idx]==-t) return 1;
+            else return 0;
+        }
+        int neg=helper(idx-1,t-nums[idx],nums);
+        int pos=helper(idx-1,t+nums[idx],nums);
+        return neg+pos;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return helper(nums.size()-1,target,nums);
+    }
+    */
+    // Converting the recursion to memoization is difficult and consumes more space
+    /*
+    // Recursion
+    int helper(int idx, int t, vector<int> &nums) {
+        if(idx==0) {
+            if(t==0 && nums[idx]==t) return 2;
+            else if(t==0 || nums[idx]==t) return 1;
+            else return 0;
+        }
+        int notTake=helper(idx-1,t,nums);
+        int take=0;
+        if(nums[idx]<=t) take=helper(idx-1,t-nums[idx],nums);
+        return take+notTake;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int s=0;
+        for(int n:nums) s+=n;
+        if(target+s<0 || (target+s)%2!=0) return 0;
+        int k=(target+s)/2;
+        return helper(nums.size()-1,k,nums);
+    }
+    */
+    
+    // Memoization
+    int helper(int idx, int t, vector<int> &nums, vector<vector<int>> &memo) {
+        if(idx==0) {
+            if(t==0 && nums[idx]==t) return 2;
+            else if(t==0 || nums[idx]==t) return 1;
+            else return 0;
+        }
+        if(memo[idx][t]!=-1) return memo[idx][t];
+        int notTake=helper(idx-1,t,nums,memo);
+        int take=0;
+        if(nums[idx]<=t) take=helper(idx-1,t-nums[idx],nums,memo);
+        return memo[idx][t]=take+notTake;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int s=0;
+        for(int n:nums) s+=n;
+        if(target+s<0 || (target+s)%2!=0) return 0;
+        int k=(target+s)/2;
+        int n=nums.size();
+        vector<vector<int>> memo(n, vector<int>(k+1,-1));
+        return helper(n-1,k,nums,memo);
+    }
+    
+};
