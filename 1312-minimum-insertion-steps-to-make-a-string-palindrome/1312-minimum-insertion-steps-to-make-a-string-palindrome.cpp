e639f1c997f6fbfad506076bd80aa5eed857cd9d
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-09-14 23:31:16 +0530

    Time: 115 ms (31.22%), Space: 26.8 MB (56.56%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    /*
+    // Tabulation
     int minInsertions(string s) {
         int n=s.size();
         vector<vector<int>> table(n+1, vector<int>(n+1,-1));
@@ -16,5 +18,26 @@ public:
             }
         }
         return s.size()-table[n][n];
+    }
+    */
+    // Space Optimization
+    int minInsertions(string s) {
+        int n=s.size();
+        vector<int> prev(n+1,-1);
+        string s1=s;
+        string s2=s;
+        reverse(s2.begin(),s2.end());
+        for(int i=0;i<=n;i++) {
+            vector<int> curr(n+1,-1);
+            for(int j=0;j<=n;j++) {
+                if(i==0 || j==0) curr[j]=0;
+                else {
+                    if(s1[i-1]==s2[j-1]) curr[j]=1+prev[j-1];
+                    else curr[j]=max(prev[j],curr[j-1]);
+                }
+            }
+            prev=curr;
+        }
+        return s.size()-prev[n];
     }
 };
