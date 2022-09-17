4ff1c3e03ee201913607ece840be779c8abb07eb
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-09-17 15:00:21 +0530

    Time: 1192 ms (24.05%), Space: 207 MB (38.93%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -16,6 +16,7 @@ public:
     }
     */
     
+    /*
     // Memoization
     int helper(int i, int buy, int trans, int n, vector<int> &prices, vector<vector<vector<int>>> &memo) {
         if(trans==0) return 0;
@@ -30,5 +31,29 @@ public:
         int n=prices.size();
         vector<vector<vector<int>>> memo(n,vector<vector<int>>(3, vector<int>(3,-1)));
         return helper(0,0,2,n,prices,memo);
+    }
+    */
+    
+    // Tabulation
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<vector<int>>> table(n+1,vector<vector<int>>(2, vector<int>(3,-1)));
+        for(int i=0;i<=n;i++) {
+            for(int buy=0;buy<2;buy++) table[i][buy][0]=0;
+        }
+        for(int buy=0;buy<2;buy++) {
+            for(int trans=1;trans<=2;trans++) table[n][buy][trans]=0;
+        }
+        for(int i=n-1;i>=0;i--) {
+            for(int buy=0;buy<=1;buy++) {
+                for(int trans=1;trans<=2;trans++) {
+                    int op1=INT_MIN, op2=INT_MIN;
+                    if(buy==0) op1=max(table[i+1][0][trans],-prices[i]+table[i+1][1][trans]);
+                    if(buy==1) op2=max(table[i+1][1][trans], prices[i]+table[i+1][0][trans-1]);
+                    table[i][buy][trans]=max(op1,op2);
+                }
+            }
+        }
+        return table[0][0][2];
     }
 };
