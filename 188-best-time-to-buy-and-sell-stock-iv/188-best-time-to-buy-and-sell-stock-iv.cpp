7dbafd057f86a4aa3c4e19682d2b8c3b0c79ba49
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-09-21 00:36:04 +0530

    Time: 32 ms (32.56%), Space: 15.5 MB (10.33%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -26,6 +26,7 @@ public:
     }
     */
     
+    /*
     // Tabulation
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
@@ -39,5 +40,23 @@ public:
             }
         }
         return table[0][0][k];
+    }
+    */
+    
+    // Space Optimization
+    int maxProfit(int k, vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> prev(2, vector<int>(k+1,0));
+        for(int i=n-1;i>=0;i--) {
+            vector<vector<int>> curr(2, vector<int>(k+1,0));
+            for(int buy=0;buy<=1;buy++) {
+                for(int tran=1;tran<=k;tran++) {
+                    if(buy==0) curr[buy][tran]=max(prev[0][tran], -prices[i]+prev[1][tran]);
+                    else curr[buy][tran]=max(prev[1][tran], prices[i]+prev[0][tran-1]);
+                }
+            }
+            prev=curr;
+        }
+        return prev[0][k];
     }
 };
