df2bb6d163d365364280d47bb883557b29ceb50c
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-09-21 11:36:02 +0530

    Time: 15 ms (20.84%), Space: 11.9 MB (48.90%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -11,6 +11,7 @@ public:
         return helper(0,0,prices);
     }*/
     
+    /*
     // Memoization
     int helper(int i, int buy, vector<int> &prices,vector<vector<int>> &memo) {
         if(i>=prices.size()) return 0;
@@ -21,5 +22,19 @@ public:
     int maxProfit(vector<int>& prices) {
         vector<vector<int>> memo(prices.size(), vector<int>(2,-1));
         return helper(0,0,prices,memo);
+    }
+    */
+    
+    // Tabulation
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> table(n+2, vector<int>(2,0));
+        for(int i=n-1;i>=0;i--) {
+            for(int buy=1;buy>=0;buy--) {
+                if(buy==0) table[i][buy]=max(table[i+1][0], -prices[i]+table[i+1][1]);
+                else table[i][buy]=max(table[i+1][1], prices[i]+table[i+2][0]);
+            }
+        }
+        return table[0][0];
     }
 };
