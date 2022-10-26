b8f76ce69704b8f011381b4042b2ed0f8d623293
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-10-26 17:33:43 +0530

    Time: 8 ms (25.58%), Space: 9.1 MB (30.72%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    /*
+    // Swapping and Insertion Sort
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         if(n==0) return;
         for(int i=0;i<m;i++) {
@@ -10,5 +12,23 @@ public:
             nums2[k-1]=first;
         }
         for(int i=0;i<n;i++) nums1[i+m]=nums2[i];
+    }
+    */
+    
+    // Gap Method
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if(n==0) return;
+        for(int i=0;i<n;i++) nums1[m+i]=nums2[i];
+        int gap=ceil((float)(m+n)/2);
+        while(gap>0) {
+            int i=0,j=gap;
+            while(j<m+n) {
+                if(nums1[i]>nums1[j]) swap(nums1[i],nums1[j]);
+                i++;
+                j++;
+            }
+            if(gap==1) break;
+            gap=ceil((float)gap/2);
+        }
     }
 };
