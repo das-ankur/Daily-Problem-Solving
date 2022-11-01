99ec056f0dc105569f7ac11d98e23024e6942682
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-11-01 17:52:32 +0530

    Time: 180 ms (39.45%), Space: 48.5 MB (26.02%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -13,6 +13,7 @@ public:
         return helper(0,nums);
     }
     */
+    /*
     // Memoization
     bool helper(int i, vector<int> &nums, vector<int> &memo) {
         if(i==nums.size()-1) return true;
@@ -29,5 +30,20 @@ public:
     bool canJump(vector<int>& nums) {
         vector<int> memo(nums.size(),-1);
         return helper(0,nums,memo);
+    }
+    */
+    // Best Approach: Greedy
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int x=n-1;
+        bool ans=0;
+        for(int i=n-1;i>=0;i--) {
+            if(i+nums[i]>=x) {
+                ans=1;
+                x=i;
+            }
+            else ans=0;
+        }
+        return ans;
     }
 };
