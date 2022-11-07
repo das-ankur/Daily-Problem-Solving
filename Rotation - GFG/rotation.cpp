6c91583a6c43578ee059ff023a97d90e57c372f3
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-11-07 15:44:37 +0530

    Added solution - LeetHub

--- Rotation - GFG/rotation.cpp
+++ Rotation - GFG/rotation.cpp
@@ -17,8 +17,8 @@ public:
 	        int prev=(mid+n-1)%n;
 	        int next=(mid+1)%n;
 	        if(arr[mid]<=arr[prev] && arr[mid]<=arr[next]) return mid;
-	        else if(arr[mid]>=arr[0]) low=mid+1;
-	        else high=mid-1;
+	        else if(arr[high]>arr[mid]) high=mid-1;
+	        else low=mid+1;
 	    }
 	    return -1;
 	}
