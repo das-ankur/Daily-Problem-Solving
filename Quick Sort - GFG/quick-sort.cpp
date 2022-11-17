a563829b63ecc79b10c34f06e00a958039125290
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-11-17 15:37:00 +0530

    Added solution - LeetHub

--- Quick Sort - GFG/quick-sort.cpp
+++ Quick Sort - GFG/quick-sort.cpp
@@ -43,6 +43,19 @@ class Solution
        return j;
     }
     */
+    /*
+    int partition(int arr[], int low, int high) {
+        int pivot=arr[low];
+        int i=low, j=high;
+        while(i<j) {
+            while(arr[i]<=pivot && i<=high-1) i++;
+            while(arr[j]>pivot && j>=low) j--;
+            if(i<j) swap(arr[i],arr[j]);
+        }
+        swap(arr[j],arr[low]);
+        return j;
+    }
+    */
     int partition(int arr[], int low, int high) {
         int pivot=arr[low];
         int i=low, j=high;
