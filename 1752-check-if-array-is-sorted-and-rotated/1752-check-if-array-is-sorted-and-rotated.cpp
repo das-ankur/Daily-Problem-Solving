a8214f84e0985f06abe5a75fc0af52fe6db49e41
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-11-20 12:18:09 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (37.15%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     bool check(vector<int>& nums) {
         int n=nums.size();
         int count=0;
@@ -7,5 +8,28 @@ public:
             if(nums[i]>nums[(i+1)%n]) count++;
         }
         return count<=1;
+    }
+    */
+    // Brute Force
+    bool isTwoArrayEqual(vector<int> &arr1, vector<int> &arr2) {
+        for(int i=0;i<arr1.size();i++) {
+            if(arr1[i]!=arr2[i]) return false;
+        }
+        return true;
+    }
+    void rotate(vector<int> &nums, int n) {
+        int t=nums[n-1];
+        for(int i=n-1;i>0;i--) nums[i]=nums[i-1];
+        nums[0]=t;
+    }
+    bool check(vector<int> &nums) {
+        int n=nums.size();
+        vector<int> temp(nums);
+        sort(temp.begin(),temp.end());
+        for(int i=0;i<n;i++) {
+            rotate(nums, n);
+            if(isTwoArrayEqual(temp,nums)) return true;
+        }
+        return false;
     }
 };
