efa89f43d8488e95d15d096b77ded412a30dcc61
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-11-20 12:37:30 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (73.72%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -10,26 +10,37 @@ public:
         return count<=1;
     }
     */
+    /*
     // Brute Force
-    bool isTwoArrayEqual(vector<int> &arr1, vector<int> &arr2) {
+    bool isTwoArrayEqual(vector<int> &arr1, vector<int> &arr2) {    // O(n)
         for(int i=0;i<arr1.size();i++) {
             if(arr1[i]!=arr2[i]) return false;
         }
         return true;
     }
-    void rotate(vector<int> &nums, int n) {
+    void rotate(vector<int> &nums, int n) {                         //O(n)
         int t=nums[n-1];
         for(int i=n-1;i>0;i--) nums[i]=nums[i-1];
         nums[0]=t;
     }
-    bool check(vector<int> &nums) {
+    bool check(vector<int> &nums) {             
         int n=nums.size();
         vector<int> temp(nums);
         sort(temp.begin(),temp.end());
-        for(int i=0;i<n;i++) {
+        for(int i=0;i<n;i++) {                                      //O(n)
             rotate(nums, n);
             if(isTwoArrayEqual(temp,nums)) return true;
         }
         return false;
+    }
+    //Total Complexcity: O(n^2)
+    */
+    // Optimal Approach
+    bool check(vector<int> &nums) {
+        int count=0;
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i]>nums[(i+1)%nums.size()]) count++;
+        }
+        return count<=1;
     }
 };
