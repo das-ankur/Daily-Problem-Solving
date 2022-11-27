0fe0f10054d38fe7a0edfba5dedf862d749db05c
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-11-27 11:53:22 +0530

    Time: 51 ms (41.77%), Space: 17.9 MB (66.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -8,6 +8,7 @@ public:
         return total_sum-array_sum;
     }
     */
+    /*
     // Naive Approach
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
@@ -16,4 +17,12 @@ public:
         for(int i=0;i<=n;i++) if(!flag[i]) return i;
         return -1;
     }
+    */
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int total_sum=n*(n+1)/2;
+        int array_sum=accumulate(nums.begin(),nums.end(),0);
+        return total_sum-array_sum;
+    }
+    
 };
