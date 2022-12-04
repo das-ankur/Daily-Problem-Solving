50e8b5639e814836886386581a934ab7cbfa7264
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-12-04 11:48:44 +0530

    Time: 45 ms (46.42%), Space: 19.7 MB (15.17%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    /*
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++) m[nums[i]]++;
+        int major, freq=INT_MIN;
+        int t=nums.size()/2;
+        for(auto x:m)  {
+            if(x.second>=t && x.second>freq) {
+                freq=x.second;
+                major=x.first;
+            }
+        }
+        return major;
+        */
+        /*
+        // Optimized Approach
+        int ans,count=0;
+        for(int i=0;i<nums.size();i++) {
+            if(count==0) {
+                ans=nums[i];
+                count=1;
+                continue;
+            }
+            if(ans!=nums[i])
+                count--;
+            else
+                count++;
+        }
+        return ans;
+    }
+    */
+    /*
+    // Practice 1
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int ans=0;
+        for(int i=0;i<nums.size();i++) {
+            if(count==0) {
+                ans=nums[i];
+                count++;
+            }
+            else {
+                if(ans==nums[i]) count++;
+                else count--;
+            }
+        }
+        return ans;
+    }
+    */
+    /*
+    int majorityElement(vector<int>& nums) {
+        int count=0, num;
+        for(int i=0;i<nums.size();i++) {
+            if(count==0) {
+                num=nums[i];
+                count++;
+            }
+            else {
+                if(num==nums[i]) count++;
+                else count--;
+            }
+        }
+        return num;
+    }
+    */
+    // Hashing
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for(int x: nums) m[x]++;
+        for(auto x: m) {
+            if(x.second>nums.size()/2) return x.first;
+        }
+        return -1;
+    }
+}; 
