5d605f50c507166b7bc6b56a9b1d10940ec79dd7
Ankur Das <43563075+das-ankur@users.noreply.github.com>
2022-12-04 11:16:28 +0530

    Time: 74 ms (14.70%), Space: 20 MB (20.03%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    /*
+    int singleNumber(vector<int>& nums) {
+        // Apply xor
+        int res=0;
+        for(int i=0;i<nums.size();i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+    */
+    // Hashing
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for(int x: nums) m[x]++;
+        for(auto x: m) {
+            // x.fiurst-> number, x.second: frequency
+            if(x.second==1) return x.first;
+        }
+        return -1;
+    }
+};
